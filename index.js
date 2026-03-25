263f494e4d53cdbfec8e9a82549fd2418fd283dd
unknown <esekodyn857@gmail.com>
2026-03-25 20:10:56 +0100

    secong commit

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-// This allows your API to read JSON data sent from a frontend
+
 app.use(express.json());
 
-// --- DATABASE CONNECTION ---
-// Replace YOUR_CONNECTION_STRING with the one from MongoDB Atlas
+
 const dbURI = "mongodb+srv://NICOLEESEKODY_db_user:stopPlayingwithem2026@CA1cluster.jpsvqsr.mongodb.net/?appName=CA1cluster";
 
 mongoose.connect(dbURI)
   .then(() => console.log("✅ Success: Connected to MongoDB Atlas!"))
   .catch(err => console.log("❌ Connection Error:", err));
 
-// --- THE BLUEPRINT (SCHEMA) ---
+
 const userSchema = new mongoose.Schema({
   username: String,
   password: { type: String, required: true }
@@ -21,7 +20,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-// --- THE API ROUTE (CREATE) ---
+
 app.post('/api/submit-form/login', async (req, res) => {
   try {
     const newUser = new User(req.body);
@@ -32,7 +31,7 @@ app.post('/api/submit-form/login', async (req, res) => {
   }
 });
 
-// --- START THE SERVER ---
+
 app.listen(3000, () => {
   console.log("🚀 Server is running on http://localhost:3000");
 });
